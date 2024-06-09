d2f6bb137d6045881490e37801609f82db099548
Juliano Costa <juliano_ciawax@hotmail.com>
2024-06-09 14:34:18 -0300

    first commit

---
+++ Java/ArraySimples.java
@@ -0,0 +1,6 @@
+public class ArraySimples {
+    public static void main(String[] args) {
+        String[] paises = {"BRASIL", "RUSSIA", "INDIA", "CHINA"};
+        System.out.println(paises[0]);
+    }
+}
---
+++ Java/Variavel.java
@@ -0,0 +1,7 @@
+public class Variavel {
+
+    public static void main (String[] args){
+        String nome = "Carlos";
+        System.out.println  (nome);
+    } //Fim do método
+} // Fim da Classe
